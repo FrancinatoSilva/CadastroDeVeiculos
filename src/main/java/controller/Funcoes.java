09fef4e3012ebe5956c5ee8e5f2a9e3b2e390c96
Francis Lean <irenofrancinato@gmail.com>
2025-04-16 20:03:13 -0300

    Adição da classe fFuncoes classe responsavel pelos métodos de cada opção do menu

---
+++ src/main/java/controller/Funcoes.java
@@ -0,0 +1,15 @@
+package controller;
+
+import java.util.Scanner;
+
+public class Funcoes {
+
+    public static void opcao1() {
+        Scanner sc = new Scanner (System.in);
+
+
+
+
+        sc.close();
+    }
+}
